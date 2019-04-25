0a5d557c106b917a9b0df493f2bb5af94c8e8db9
xiaobiezhii <571561827@qq.com>
2019-04-25 22:57:48 +0800

    vue-cli开荒、简单路由

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<h1>======我是公共的部分======</h1>
+		<router-view></router-view>
+		<h1>======我是公共的部分======</h1>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
 
